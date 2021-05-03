c6011656dffc39fdafba2321fd247ee5fd15006f
ambergoutam <65751359+ambergoutam@users.noreply.github.com>
2021-05-03 11:40:51 +0530

    Create division.js

---
+++ Web Technology/jQuery/Three Divisions/division.js
@@ -0,0 +1,6 @@
+//WRITE YOUR jQUERY CODE HER
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $("div[name$='Intelligence']").css("background-color","yellow");
+    });
+});
